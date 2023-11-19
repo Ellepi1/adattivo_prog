bee25b5d5267c78a932a7a62b3df34e3012cfd62
leo <leopoldolpmanna@gmail.com>
2023-11-19 18:25:56 +0100

    creati factory e seed

---
+++ adattivo_prog/database/factories/PersonFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Person;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PersonFactory extends Factory
+{ 
+    protected $model = Person::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nome' => $this->faker->name(),
+ 	        'data di nascita' => $this->faker->date(),
+        ];
+    }
+}
